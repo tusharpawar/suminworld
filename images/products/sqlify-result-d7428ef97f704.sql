c7eb644b1c3584cb173474a1c9b97bd8d7753ae1
Tushar Pawar <tusharpawar2012@gmail.com>
2019-12-22 14:04:16 +0530

    Added files

---
+++ images/products/sqlify-result-d7428ef97f704.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS data (
+component_name VARCHAR(23) NULL,
+component_version VARCHAR(8) NULL,
+lastmodifiedon VARCHAR(28) NULL
+);
+
+INSERT INTO data VALUES
+("platform-agent-core.exe","1.0.1418","2018-10-26 15:16:18.000+0000"),
+("platform-agent-core.exe","1.0.1418","2018-10-26 15:16:18.000+0000");
